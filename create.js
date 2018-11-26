e971e1acd878aef990e2eed52b0715b197c72b39
thegreatsantini <lss13@my.fsu.edu>
2018-11-25 17:47:56 -0800

    created create lambda func

---
+++ create.js
@@ -0,0 +1,38 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+
+// const credentials = new AWS.SharedIniFileCredentials({ profile: 'JotBox' }); AWS.config.credentials = credentials;
+
+
+export async function main(event, context, callback) {
+    // Request body is passed in as a JSON encoded string in 'event.body'
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: "drafts",
+        // 'Item' contains the attributes of the item to be created
+        // - 'userId': user identities are federated through the
+        //             Cognito Identity Pool, we will use the identity id
+        //             as the user id of the authenticated user
+        // - 'noteId': a unique uuid
+        // - 'content': parsed from request body
+        // - 'attachment': parsed from request body
+        // - 'createdAt': current Unix timestamp
+        Item: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            draftId: uuid.v1(),
+            title: data.title,
+            draft: data.draft,
+            notes: data.notes,
+            createdAt: Date.now()
+        }
+    };
+
+    try {
+        await dynamoDbLib.call("put", params);
+        return success(params.Item);
+    } catch (e) {
+        return failure({ status: false });
+    }
+}
