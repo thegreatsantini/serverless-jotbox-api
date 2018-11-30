4b127e336b7c01e33104b013b7e2d707974b2e9d
thegreatsantini <lss13@my.fsu.edu>
2018-11-29 16:05:34 -0800

    created addUser lamdba function

---
+++ addUser.js
@@ -0,0 +1,35 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+
+// const credentials = new AWS.SharedIniFileCredentials({ profile: 'JotBox' }); AWS.config.credentials = credentials;
+
+
+export async function main(event, context) {
+    // Request body is passed in as a JSON encoded string in 'event.body'
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: "users",
+        // 'Item' contains the attributes of the item to be created
+        // - 'userId': user identities are federated through the
+        //             Cognito Identity Pool, we will use the identity id
+        //             as the user id of the authenticated user
+        // - 'noteId': a unique uuid
+        // - 'content': parsed from request body
+        // - 'attachment': parsed from request body
+        // - 'createdAt': current Unix timestamp
+        Item: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            userName: data.userName,
+            followers: data.followers
+        }
+    };
+
+    try {
+        await dynamoDbLib.call("put", params);
+        return success(params.Item);
+    } catch (e) {
+        return failure({ status: false });
+    }
+}
